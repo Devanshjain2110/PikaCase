90112b7ac008bfa4c614b729a2935642f732c5b7
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-06-05 21:04:38 +0530

    Made ui for file upload logic and wrote logic for dropzone

---
+++ src/app/configure/layout.tsx
@@ -0,0 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import { ReactNode } from "react"
+
+
+function Layout({children} : {children: ReactNode}) {
+  return (
+    <MaxWidthWrapper className="flex-1 flex flex-col ">
+      {children}
+    </MaxWidthWrapper>
+  )
+}
+
+export default Layout
